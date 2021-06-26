75100f45ab847062689f8adea0f986f9c586e59f
Sam Morgen <samorgen14@gmail.com>
2021-06-25 19:34:40 -0700

    Added inquirer and test question

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What would you like the title of the README to be?",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  console.log("filename is " + fileName);
+  console.log("Data is " + data.title);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  return inquirer.prompt(questions);
+}
 
 // Function call to initialize app
-init();
+init().then((data) => {
+  return writeToFile("README.md", data);
+});
